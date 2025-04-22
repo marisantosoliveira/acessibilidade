58d58197cd2623f4bf24f705b3d974494cab13d5
marisantosoliveira <oliveira.mariana28@escola.pr.gov.br>
2025-04-22 09:10:21 -0300

    .

--- script.js
+++ script.js
@@ -15,10 +15,6 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
-    alternaContraste.addEventListener('click', function(){
-        document.body.classList.toggle('alto-contraste')
-})
-
 
 });
 
